4d3edd3386834021a2f2d52930b9c80d15f8419a
Tanner Donovan <ttdonovan@gmail.com>
2015-06-18 08:32:10 -0700

    add search to CharactersIndexRoute
    * create new CharactersIndexController that defines query params
    * updated the CharactersIndexRoute to filter on query params
    * add search input field and button to characters template

--- app/routes/characters/index.js
+++ app/routes/characters/index.js
@@ -1,7 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model() {
-    return this.store.find('character');
+  queryParams: {
+    search: {
+      refreshModel: true
+    }
+  },
+
+  model(params) {
+    if (Ember.isPresent(params.search)) {
+      return this.store.find('character', {
+        nameStartsWith: params.search
+      });
+    } else {
+      return this.store.find('character');
+    }
+  },
+
+  actions: {
+    search() {
+      this.transitionTo('characters.index');
+    }
   }
 });
--- tests/acceptance/characters-test.js
+++ tests/acceptance/characters-test.js
@@ -18,12 +18,25 @@ test('visiting /', function(assert) {
   assert.expect(2);
 
   visit('/');
-  click('a:contains(Characters)');
+  click('a:contains("Characters")');
 
   andThen(function() {
     assert.equal(currentURL(), '/characters');
 
     const characters = find('.character');
     assert.equal(characters.length, 20);
+  });
+});
+
+test('visiting /characters and search for \'Iron Man\`', function(assert) {
+  assert.expect(2);
+
+  visit('/characters');
+  fillIn('input:text', 'Iron Man');
+  click('input:submit');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/characters?search=Iron%20Man');
+    assert.equal(find('.character:contains("Iron Man (Marvel Heroes)")').length, 1);
   });
 });
